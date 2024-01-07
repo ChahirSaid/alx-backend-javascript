f925a1eb207a2e040b61dbd65a3fed1d3d1bf97a
ChahirSaid <chahirsaid18@gmail.com>
2024-01-07 19:47:04 +0100

    11. EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,10 +4,13 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+    if (typeof range !== 'string') {
+      throw TypeError('Range must be a string');
+    }
     this._range = range;
   }
 
-  cloneCar() {
-    return new Car();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
